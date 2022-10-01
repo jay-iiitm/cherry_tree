c9bfb014295609041442ec2af31c162909088981
Niketan0109 <56088377+Niketan0109@users.noreply.github.com>
2022-10-01 12:54:39 +0530

    Update cc.cpp

--- cc.cpp
+++ cc.cpp
@@ -51,7 +51,25 @@ int binarySearch(int arr[],int l,int r,int x)
     return -1;
 } 
  
- 
+ class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       int l=0;
+       int r = arr.size()-1;
+       while(r-l>=k)
+       {
+          if(x-arr[l]<=arr[r]-x)
+          {
+              r--;
+          }
+           else{
+               l++;
+           }
+       }
+        return vector<int>(begin(arr)+l,begin(arr)+r+1);
+    }
+};
+
 int fact(int n,int k)       //for calculating nCk
 {
     int ans=1;
